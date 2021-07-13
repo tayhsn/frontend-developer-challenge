7bfe5187f2d97660f1d9581d6812d1c22e1584d2
Tayanne <tayh.snovaes@gmail.com>
2021-07-13 10:18:26 -0300

    improve validations

--- scripts/validator.js
+++ scripts/validator.js
@@ -1,29 +1,80 @@
 /**
  * This script have the following VALIDATION responsabilities:
- * 1_ Do the check of the inputs NAME, EMAIL, CPF by buttons' events
+ * Do the check of the inputs NAME, EMAIL, CPF by buttons' events
  */
 
-/** 1_ BUTTON SEND in SECTION FORM ALGORITHM */
- var btnSend = document.querySelector('#send');
- btnSend.addEventListener('click', (e) => {
-	 e.preventDefault();
+/** BUTTON SEND in SECTION FORM ALGORITHM */
+const btnSend = document.querySelector('#send');
+btnSend.addEventListener('click', (e) => {
+	e.preventDefault();
 
-	let name = document.getElementById('name').value;
-	let email = document.getElementById('email').value;
-	let cpf = document.getElementById('cpf').value;
+	var inputName = document.getElementById('name');
+	var inputEmail = document.getElementById('email');
+	var inputCpf = document.getElementById('cpf');
 
-	if(name == '' || email == '' || cpf == ''){
-		alert('Por favor, preencha todos os campos');
+	if (inputName.value === '') {
+		inputName.classList.add('error');
+		alert('Por favor, preencha todos os campos!');
 		return false;
-	} 
+	} else {
+		inputName.classList.remove('error');
+	}
 
-	alert('Obrigado por colaborar com nosso algoritmo!')
- });
- 
- /**  1_ BUTTON SEND NOW in FORM SHARE */
-var btnSendNow = document.querySelector('#sendNow');
-btnSendNow.addEventListener('click', () => {
-	
-	alert('Email enviado com sucesso. Muito obrigado!')
+	if (
+		inputEmail.value === '' ||
+		inputEmail.value.indexOf('@') == -1 ||
+		inputEmail.value.indexOf('.') == -1
+	) {
+		inputEmail.classList.add('error');
+		alert('Por favor, preencha todos os campos!');
+		return false;
+	} else {
+		inputEmail.classList.remove('error');
+	}
+
+	if (inputCpf.value === '' || inputCpf.value < 11) {
+		inputCpf.classList.add('error');
+		alert('Por favor, preencha todos os campos!');
+		return false;
+	} else {
+		inputCpf.classList.remove('error');
+	}
+
+	alert('Obrigado por colaborar com nosso algoritmo!');
 });
 
+
+
+/**  BUTTON SEND NOW in FORM SHARE */
+const btnSendNow = document.querySelector('#sendNow');
+btnSendNow.addEventListener('click', (e) => {
+	e.preventDefault();
+	
+	var inputNameFriend = document.getElementById('name-friend');
+	var inputEmailFriend = document.getElementById('email-friend');
+
+	if (inputNameFriend.value === '') {
+		inputNameFriend.classList.add('error');
+		alert('Por favor, preencha todos os campos!');
+		return false;
+	} else {
+		inputNameFriend.classList.remove('error');
+	}
+
+	if (
+		inputEmailFriend.value === '' ||
+		inputEmailFriend.value.indexOf('@') == -1 ||
+		inputEmailFriend.value.indexOf('.') == -1
+	) {
+		inputEmailFriend.classList.add('error');
+		alert('Por favor, preencha todos os campos!');
+		return false;
+	} else {
+		inputEmailFriend.classList.remove('error');
+	}
+
+
+	alert('Email enviado com sucesso. Muito obrigado!');
+
+	window.open('../views/email.html', '_blank');
+});
