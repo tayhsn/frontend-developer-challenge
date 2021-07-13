9a0b62d6ae0eec92249d4806f420279ccf7d604d
Tayanne <tayh.snovaes@gmail.com>
2021-07-12 22:28:39 -0300

    add basic validation

--- scripts/validator.js
+++ scripts/validator.js
@@ -1,19 +1,29 @@
 /**
- * This script have the following responsabilities about VALIDATION and GENERICS:
+ * This script have the following VALIDATION responsabilities:
  * 1_ Do the check of the inputs NAME, EMAIL, CPF by buttons' events
  */
 
 /** 1_ BUTTON SEND in SECTION FORM ALGORITHM */
  var btnSend = document.querySelector('#send');
- btnSend.addEventListener('click', () => {
-	console.log('cliquei')
-	 //FAZER A VALIDAÇÃO DO PRIMEIRO FORMS
+ btnSend.addEventListener('click', (e) => {
+	 e.preventDefault();
+
+	let name = document.getElementById('name').value;
+	let email = document.getElementById('email').value;
+	let cpf = document.getElementById('cpf').value;
+
+	if(name == '' || email == '' || cpf == ''){
+		alert('Por favor, preencha todos os campos');
+		return false;
+	} 
+
+	alert('Obrigado por colaborar com nosso algoritmo!')
  });
  
  /**  1_ BUTTON SEND NOW in FORM SHARE */
 var btnSendNow = document.querySelector('#sendNow');
 btnSendNow.addEventListener('click', () => {
-	console.log('cliquei')
-	//FAZER A VALIDAÇÃO DO SEGUNDO FORMS
+	
+	alert('Email enviado com sucesso. Muito obrigado!')
 });
 
